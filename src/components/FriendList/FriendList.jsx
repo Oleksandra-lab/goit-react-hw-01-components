5e0737db7db924d714be780b72850cfcd3cb8459
Oleksandra-lab <127051632+Oleksandra-lab@users.noreply.github.com>
2023-09-17 18:16:38 +0300

    Profile is styled, styled files added

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,8 +3,13 @@ import { } from './FriendList.styled'
 
 const FriendsList = ( { friends }) => (
     <ul className="friend-list">
-        {friends.map(({avatar, name, isOnLine}) =>
-        (<li className="item">
+        {friends.map(({id, avatar, name, isOnLine}) =>
+        (<li 
+          key = {id}
+          avatar = {avatar}
+          name = {name}
+          isOnLine = {isOnLine}
+        className="item">
         <span className="status">{isOnLine}</span>
         <img className="avatar" src={avatar} alt={name} width="48" />
         <p className="name">{name}</p>
